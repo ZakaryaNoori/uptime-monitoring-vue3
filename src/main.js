f7599d9273f6eec3ad9ad88378e301bb1434dcb9
ZakaryaNoori <zakaryanoori979@gmail.com>
2021-01-19 10:33:37 +0430

    Configure styles

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// import router
 import router from './router'
+
+// import styles
+import './assets/scss/app.scss'
 
 createApp(App).use(router).mount('#app')
